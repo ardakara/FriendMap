89f5c2287d62cbc002bd0051cc2ca03f82c4c99c
Arda Kara <ardakara@gmail.com>
2011-11-01 10:23:36 -0700

    testing server access

--- data.js
+++ data.js
@@ -2,38 +2,34 @@ function login()
 {
 	FB.login(function(response) {
 		if (response.authResponse) {
-			alert('Welcome!  Fetching your information.... ');
+			console.log('Welcome!  Fetching your information.... ');
      	FB.api('/me', function(response) {
-      	alert('Good to see you, ' + response.name + '.');
+      	console.log('Good to see you, ' + response.name + '.');
       	FB.logout(function(response) {
-      		alert('Logged out.');
+      		console.log('Logged out.');
       	});
     	});
 		}
- 	}, {scope: 'email'});
+ 	}, {scope: 'email, friends_location, user_location'});
 	
 	FB.api('/me', function(response) {
 		$("#content").append(response.name);
 	});
+	
+	FB.api('/me/friends?fields=id,name,location,link,picture', function(response) {
+	 	facebookData = response;
+		// put in global, get in maps, geocode, then overlay as markers, then collect markers together
+	})
 }
 
 function getFriends()
 {
-	// FB.api('/me/friends', { limit: 3 }, function(response) {
-	// 	// var i = 0;
-	// 	// for (var i=0, l=response[0].length; i<l; i++) {
-	// 	// 	var friend = response[0][i];
-	// 	// 	if (friend.name) {
-	// 	// 		$("#content").append(response[0][i].name);
-	// 	// 	}
-	// 	// }
-	// 	alert(response.length);
-	// });
-	
-	FB.api('/me/friends?fields=id,name,location', function(response) {
-	 	for (var i=0, l=response.data.length; i<l; i++) {
+	FB.api('/me/friends?fields=id,name,location,link,picture', function(response) {
+		for (var i=0, l=response.data.length; i<l; i++) {
 			var friend = response.data[i];
-	 		$("#content").append(friend.name);
+			if (friend.location && friend.location.name) {
+				$("#content").append(friend.location.name);
+			}
 	 	}
 	})
 }
