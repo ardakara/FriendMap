47532b920fe3f564e06747c1979c05546618d00a
Arda Kara <ardakara@gmail.com>
2011-11-01 21:37:24 -0700

    v0.1

--- data.js
+++ data.js
@@ -15,15 +15,10 @@ function login()
 	FB.api('/me', function(response) {
 		$("#content").append(response.name);
 	});
-	
-	FB.api('/me/friends?fields=id,name,location,link,picture', function(response) {
-	 	facebookData = response;
-		// put in global, get in maps, geocode, then overlay as markers, then collect markers together
-	})
 }
 
 function getFriends()
-{
+{	
 	FB.api('/me/friends?fields=id,name,location,link,picture', function(response) {
 		for (var i=0, l=response.data.length; i<l; i++) {
 			var friend = response.data[i];
@@ -32,4 +27,75 @@ function getFriends()
 			}
 	 	}
 	})
+}
+
+// display markerclusterer markers -> look at any options available
+// add tooltips (link, photo, name)
+// one more thing (pick networks?)
+
+//Notes:
+// might have to copy friends (uncomment)
+
+var friends = [];
+var locations = new Array();
+var seenLocations = {};
+var markers = [];
+var callbackCounter = -1;
+function populateArray() 
+{
+	FB.api('/me/friends?fields=id,name,location,link,picture', function(response) {
+		for (var i=0, l=response.data.length; i<l; i++) {
+			//var friend = jQuery.extend(true, {}, response.data[i]);
+			var friend = response.data[i];
+			
+			if (!friend.location || !friend.location.id) {
+				continue;
+			}
+			
+			if (seenLocations[friend.location.id]) {
+				//location is known
+			} else {
+				seenLocations[friend.location.id] = true;
+				if (callbackCounter == -1) {
+					callbackCounter = 0;
+				}
+				callbackCounter++;
+				FB.api('/' + friend.location.id, function(locationResponse) {
+					if (locationResponse.location) {
+						locations[locationResponse.id] = locationResponse.location;
+					}
+					callbackCounter--;
+				});
+			}
+			friends.push(friend);
+		}
+	});
+}
+
+function putMarkers()
+{
+	while (callbackCounter != 0) {
+		setTimeout("putMarkers()", 1000);
+		return;
+	}
+	for (i in friends) {
+		var friend = friends[i];
+		if (locations[friend.location.id]) {
+			var lat_lng = new google.maps.LatLng(locations[friend.location.id].latitude, locations[friend.location.id].longitude);
+			var marker = new google.maps.Marker({
+				//map:map,
+				position: lat_lng,
+			});
+			markers.push(marker);
+			// add like the botton one again
+		}
+	}
+	//var mcOptions = {gridSize: 50, maxZoom: 15};
+	var mc = new MarkerClusterer(map, markers);
+}
+
+function mapFriends()
+{
+	populateArray();
+	putMarkers();
 }
